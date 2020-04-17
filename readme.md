0f8f83a93dfd6be5e117bd248e6a1cb0102d4d9b
Nicole OBomsawin <nicoleobomsawin@gmail.com>
2020-04-17 14:10:34 -0400

    initial commit

---
+++ readme.md
@@ -0,0 +1,47 @@
+## Quarantini
+
+### Description
+```
+What better way to solve quarantine boredom than to have an app figure out what to do for you?
+The Quarantini app can mix you a drink, tell you a joke, and even inspire you.
+I mean, what else are you doing with your time?
+```
+## User Story
+```
+AS A quarantined member of society
+I WANT to find something I haven't done before
+SO THAT I can occupy my time
+```
+
+## Acceptance Criteria
+```
+WHEN I enter the site
+THEN I am presented with six options
+WHEN I choose the drink option
+THEN I am presented with a random drink
+WHEN I click on that drink
+THEN I am directed to a page on how to create that drink
+WHEN I click on the joke option
+THEN I am presented with a random joke
+WHEN I click on the bored option
+THEN I am presented with a random activity
+WHEN I click on the inspiration link
+THEN I am presented with a random inspirational quote
+WHEN I click on the movie button
+THEN I am presented with a random movie title and its rating
+WHEN I click on the predict button
+THEN I am presented with a random number of days until quarantine ends
+WHEN I click the back button
+THEN I get back to the homepage
+```
+
+## APIs Used
+```
+* TMBD // https://developers.themoviedb.org/3
+* The Cocktail DB // https://www.thecocktaildb.com/api.php
+* Bored API // http://www.boredapi.com/api/activity
+* Type Fit API // https://type.fit/api/quotes
+```
+
+## Deployed at
+https://nicoleobom.github.io/quarantini
---
+++ script.js
@@ -0,0 +1,140 @@
+if (top.location.pathname === "/drink.html") {
+    $(document).ready(function() {
+        
+        $.ajax({
+            url: "https://www.thecocktaildb.com/api/json/v1/1/random.php",
+            method: "GET"
+        }).then(function(response){
+
+            var drinkIng = '';
+
+            if (response.drinks[0].strIngredient1 != null) {
+                drinkIng = drinkIng + response.drinks[0].strIngredient1 + "<br>";
+            }
+
+            if (response.drinks[0].strIngredient2 != null) {
+                drinkIng = drinkIng + response.drinks[0].strIngredient2 + "<br>";
+            }
+
+            if (response.drinks[0].strIngredient3 != null) {
+                drinkIng = drinkIng + response.drinks[0].strIngredient3 + "<br>";
+            }
+
+            if (response.drinks[0].strIngredient4 != null) {
+                drinkIng = drinkIng + response.drinks[0].strIngredient4 + "<br>";
+            }
+
+            if (response.drinks[0].strIngredient5 != null) {
+                drinkIng = drinkIng + response.drinks[0].strIngredient5 + "<br>";
+            }
+
+            if (response.drinks[0].strIngredient6 != null) {
+                drinkIng = drinkIng + response.drinks[0].strIngredient6 + "<br>";
+            }
+
+            if (response.drinks[0].strIngredient7 != null) {
+                drinkIng = drinkIng + response.drinks[0].strIngredient7 + "<br>";
+            }
+
+            if (response.drinks[0].strIngredient8 != null) {
+                drinkIng = drinkIng + response.drinks[0].strIngredient8 + "<br>";
+            }
+
+            var drink = `<a href="https://www.google.com/search?q=how+to+make+a+${response.drinks[0].strDrink}+drink" target="_blank"><h2>${response.drinks[0].strDrink}</h2>
+                         <h3 id="alcoholic">${response.drinks[0].strAlcoholic}</h3>
+                         <span class="recipe"><p>${drinkIng}</p></span></a>
+                        `
+            $(".centered").empty();
+            $(".centered").append(drink);
+        });
+    })
+} else if (top.location.pathname === "/joke.html") {
+    $(document).ready(function() {
+
+        $.ajax({
+            url: "https://official-joke-api.appspot.com/random_joke",
+            method: "GET"
+        }).then(function(response) {
+
+            var joke = `<p class="joke">${response.setup}<br>
+                        ${response.punchline}</p>
+                        `
+
+            $(".centered").empty();
+            $(".centered").append(joke);
+
+        })
+
+    })
+} else if (top.location.pathname === "/bored.html") {
+    $(document).ready(function() {
+
+        $.ajax({
+            url: "http://www.boredapi.com/api/activity?type=relaxation&type=education&type=recreational&type=cooking&type=busywork&price=0.0",
+            method: "GET"
+        }).then(function(response) {
+
+            var activity = `<h2>${response.activity}</h2>
+                            `
+
+            $(".centered").empty();
+            $(".centered").append(activity);
+
+        })
+    })
+} else if (top.location.pathname === "/quote.html") {
+    $(document).ready(function() {
+
+        $.ajax({
+            url: "https://type.fit/api/quotes",
+            method: "GET"
+        }).then(function(response) {
+
+            var quotes = JSON.parse(response);
+            console.log(quotes[0].text)
+        
+            var num = Math.floor(Math.random() * 1600) + 1;
+
+            var quoteOnScreen = `<h3>${quotes[num].text}</h3>
+                                `
+
+            $(".centered").empty();
+            $(".centered").append(quoteOnScreen);
+        })
+    })
+} else if (top.location.pathname === "/movie.html") {
+    $(document).ready(function() {
+
+        var genres = [28, 12, 16, 35, 80, 99, 18, 10751, 14, 36, 27, 1042, 9648, 10749, 878, 10770, 53, 10752, 37];
+        var page = Math.floor(Math.random() * 500) + 1;
+
+        var randomGenre = genres[Math.floor(Math.random() * genres.length)];
+        console.log(randomGenre);
+
+        $.ajax({
+            url: "https://api.themoviedb.org/3/discover/movie?with_genres=" + randomGenre + "&api_key=e8f1cf6169288a814923ee8e5fe9e6f9&page=" + page,
+            method: "GET"
+        }).then(function(response) {
+
+            var movieArray = response.results;
+
+            var randomMovie = Math.floor(Math.random() * movieArray.length) + 1;
+
+            var movie = `<h3>${movieArray[randomMovie].title}</h3>
+                                <p>${movieArray[randomMovie].vote_average}/10</p>
+                                `
+            $(".centered").empty();
+            $(".centered").append(movie);
+        })
+    })
+} else if (top.location.pathname === "/predict.html") {
+    $(document).ready(function() {
+
+        var randomAmt = Math.floor(Math.random() * 2000) + 90;
+
+        var days = `<h2>${randomAmt} days</h2>
+                    `
+            $(".centered").empty();
+            $(".centered").append(days);
+    })
+}
