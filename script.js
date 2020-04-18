d455d43e2e38d68708a5d53c72f8179e292d2fdb
Nicole OBomsawin <nicoleobomsawin@gmail.com>
2020-04-17 20:05:41 -0400

    adjusting path

--- script.js
+++ script.js
@@ -102,7 +102,7 @@ if (top.location.pathname === "/Quarantini/drink.html") {
             $(".centered").append(quoteOnScreen);
         })
     })
-} else if (top.location.pathname === "/movie.html") {
+} else if (top.location.pathname === "/Quarantini/movie.html") {
     $(document).ready(function() {
 
         var genres = [28, 12, 16, 35, 80, 99, 18, 10751, 14, 36, 27, 9648, 10749, 878, 10770, 53, 10752, 37];
